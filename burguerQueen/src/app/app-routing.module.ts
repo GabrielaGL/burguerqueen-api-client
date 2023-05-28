bbd04828e0a9690abd32edf3d87cf4d602019c31
Gaabs <ggaabrielagomez@gmail.com>
2023-05-27 21:31:28 -0600

    agregar rutas de componentes a router

--- burguerQueen/src/app/app-routing.module.ts
+++ burguerQueen/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MenuWaitressComponent } from './menu-waitress/menu-waitress.component';
+import { MenuLunchWaitressComponent } from './menu-lunch-waitress/menu-lunch-waitress.component';
+import { OrdersWaitressComponent } from './orders-waitress/orders-waitress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'login', pathMatch:'full' },
+  { path:'login', component:LoginComponent },
+  { path:'menu/breakfast', component:MenuWaitressComponent, pathMatch:'full' },
+  { path:'menu/lunch', component:MenuLunchWaitressComponent, pathMatch:'full'},
+  { path:'orders', component:OrdersWaitressComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
