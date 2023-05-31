7073886ae8185988bcd6c8a1a889c17a7e559f93
Gaabs <ggaabrielagomez@gmail.com>
2023-05-31 14:47:46 -0600

    Ordenar componentes y actuazliación de rutas

--- burguerQueen/src/app/app-routing.module.ts
+++ burguerQueen/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { MenuWaitressComponent } from './components/menu-waitress/menu-waitress.component';
 import { MenuLunchWaitressComponent } from './components/menu-lunch-waitress/menu-lunch-waitress.component';
 import { OrdersWaitressComponent } from './components/orders-waitress/orders-waitress.component';
-import { AddWaitressComponent } from './add-waitress/add-waitress.component';
+import { AddWaitressComponent } from './components/add-waitress/add-waitress.component';
 import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
