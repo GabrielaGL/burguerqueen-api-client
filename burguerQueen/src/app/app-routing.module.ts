f8350348446986446a47dcf01225bbd4c255d87a
Gaabs <ggaabrielagomez@gmail.com>
2023-05-30 23:39:50 -0600

    añadir rutas de navegacion de admin

--- burguerQueen/src/app/app-routing.module.ts
+++ burguerQueen/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MenuWaitressComponent } from './menu-waitress/menu-waitress.component';
 import { MenuLunchWaitressComponent } from './menu-lunch-waitress/menu-lunch-waitress.component';
 import { OrdersWaitressComponent } from './orders-waitress/orders-waitress.component';
 import { AddWaitressComponent } from './add-waitress/add-waitress.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'login', pathMatch:'full' },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path:'menu/breakfast', component:MenuWaitressComponent, pathMatch:'full' },
   { path:'menu/lunch', component:MenuLunchWaitressComponent, pathMatch:'full'},
   { path:'orders', component:OrdersWaitressComponent },
-  { path:'add/waitress', component:AddWaitressComponent }
+  { path:'add/waitress', component:AddWaitressComponent },
+  { path:'add/products', component:ProductsComponent }
 ];
 
 @NgModule({
