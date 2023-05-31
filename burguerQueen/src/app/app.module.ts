0dfb3d4e280d64bd06939347ffa47eb5595880cf
Gaabs <ggaabrielagomez@gmail.com>
2023-05-30 18:46:57 -0600

    agregar etiqueta formsModule para uso del form login

--- burguerQueen/src/app/app.module.ts
+++ burguerQueen/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuWaitressComponent } from './menu-waitress/menu-waitress.component';
 import { OrdersWaitressComponent } from './orders-waitress/orders-waitress.component';
 import { MenuLunchWaitressComponent } from './menu-lunch-waitress/menu-lunch-waitress.component';
+import { AddWaitressComponent } from './add-waitress/add-waitress.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,13 @@ import { MenuLunchWaitressComponent } from './menu-lunch-waitress/menu-lunch-wai
     LoginComponent,
     MenuWaitressComponent,
     OrdersWaitressComponent,
-    MenuLunchWaitressComponent
+    MenuLunchWaitressComponent,
+    AddWaitressComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
