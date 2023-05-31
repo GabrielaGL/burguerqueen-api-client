91ffe80120de82f100530a72f12eb0aa05e2cb7d
Gaabs <ggaabrielagomez@gmail.com>
2023-05-30 20:11:21 -0600

    Servicio de autenticación hace la petición a la api

--- burguerQueen/src/app/services/auth.service.ts
+++ burguerQueen/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LoginI } from '../models/login.interface';
+import { ResponseI } from '../models/response.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  url: string = 'https://virtserver.swaggerhub.com/ssinuco/BurgerQueenAPI/2.0.0/'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loginByEmail(form: LoginI):Observable<ResponseI> {
+    let user = this.url + 'login';
+    return this.http.post<ResponseI>(user, form);
+  }
 }
