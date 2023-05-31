20e38d9f51f51c463e409fb6b21539cbc107939d
Gaabs <ggaabrielagomez@gmail.com>
2023-05-30 20:58:35 -0600

    añadir ruta para navegar add waitress

--- burguerQueen/src/app/app-routing.module.ts
+++ burguerQueen/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { MenuWaitressComponent } from './menu-waitress/menu-waitress.component';
 import { MenuLunchWaitressComponent } from './menu-lunch-waitress/menu-lunch-waitress.component';
 import { OrdersWaitressComponent } from './orders-waitress/orders-waitress.component';
+import { AddWaitressComponent } from './add-waitress/add-waitress.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'login', pathMatch:'full' },
   { path:'login', component:LoginComponent },
   { path:'menu/breakfast', component:MenuWaitressComponent, pathMatch:'full' },
   { path:'menu/lunch', component:MenuLunchWaitressComponent, pathMatch:'full'},
-  { path:'orders', component:OrdersWaitressComponent }
+  { path:'orders', component:OrdersWaitressComponent },
+  { path:'add/waitress', component:AddWaitressComponent }
 ];
 
 @NgModule({
