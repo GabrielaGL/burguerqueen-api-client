e4bf4c657854e0c324133dc67c90bb3c947a99f9
Gaabs <ggaabrielagomez@gmail.com>
2023-05-30 20:11:01 -0600

    Config de función para formulario enlazado con interface

--- burguerQueen/src/app/login/login.component.ts
+++ burguerQueen/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginI } from '../models/login.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+
+  constructor(private authLog:AuthService, private router:Router) {}
+
+  ngOnInit(): void {
+
+  }
+
+  onLogin(form: LoginI) {
+    this.authLog.loginByEmail(form).subscribe(data => {
+      console.log(data);
+      
+    })
+    
+  }
 
 }
