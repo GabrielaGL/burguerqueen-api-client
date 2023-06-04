0a09c8eed8b3ecb227f2ddbc637052f28265e62f
Gaabs <ggaabrielagomez@gmail.com>
2023-06-04 00:10:35 -0600

    Se muestra lista de admin con diseño

--- burguerQueen/src/app/components/add-admin/add-admin.component.ts
+++ burguerQueen/src/app/components/add-admin/add-admin.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { OrdersService } from '../../services/admin.service';
+import { workersI } from 'src/app/models/workers.interface';
+
 @Component({
   selector: 'app-add-admin',
   templateUrl: './add-admin.component.html',
   styleUrls: ['./add-admin.component.sass']
 })
 export class AddAdminComponent {
+  workers: workersI[] = [];
+  filteredWorkers: workersI[] = [];
 
+
+  constructor(private api: OrdersService) { }
+
+  ngOnInit(): void {
+    this.api.getWorkers().subscribe(data => {
+      this.workers = data
+      this.filteredWorkers = data.filter(worker => worker.role === "admin");
+      return
+    })
+  }
 }
