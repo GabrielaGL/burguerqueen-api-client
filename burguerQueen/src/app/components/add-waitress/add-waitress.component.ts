bdd7aa1c0d8a1bc8318806debf56eff88e293c58
Gaabs <ggaabrielagomez@gmail.com>
2023-06-03 23:57:20 -0600

    Lista de meseros se muestra en la vista

--- burguerQueen/src/app/components/add-waitress/add-waitress.component.ts
+++ burguerQueen/src/app/components/add-waitress/add-waitress.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { OrdersService } from '../../services/admin.service';
+import { workersI } from 'src/app/models/workers.interface';
+
 @Component({
   selector: 'app-add-waitress',
   templateUrl: './add-waitress.component.html',
   styleUrls: ['./add-waitress.component.sass']
 })
 export class AddWaitressComponent {
+  workers: workersI[] = [];
+  filteredWorkers: workersI[] = [];
 
+
+  constructor(private api: OrdersService) { }
+
+  ngOnInit(): void {
+    this.api.getWorkers().subscribe(data => {
+      this.workers = data
+      this.filteredWorkers = data.filter(worker => worker.role === "waitress");
+      return
+    })
+  }
 }
