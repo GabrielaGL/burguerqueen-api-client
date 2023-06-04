530c2176cb173d44d4a626f4103c90c268ec74a4
Gaabs <ggaabrielagomez@gmail.com>
2023-06-03 22:14:26 -0600

    Petición con productos filtrados por tipo

--- burguerQueen/src/app/components/add-products/add-products.component.ts
+++ burguerQueen/src/app/components/add-products/add-products.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../../services/admin.service';
 
+import { productsI } from 'src/app/models/products.interface';
+
 @Component({
   selector: 'app-add-products',
   templateUrl: './add-products.component.html',
   styleUrls: ['./add-products.component.sass']
 })
 export class AddProductsComponent {
+
+  products:productsI[] = [];
+  filteredProducts: productsI[] = [];
+   
   constructor(private api:OrdersService) {}
 
   ngOnInit():void {
     this.api.getProducts().subscribe(data => {
-      console.log(data);
+      this.products = data
+      this.filteredProducts = data.filter(product => product.type === "Desayuno");     
       return
     })
   }
