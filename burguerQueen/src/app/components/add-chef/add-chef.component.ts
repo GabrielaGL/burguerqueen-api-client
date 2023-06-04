17fa08c7b17b98d928f094e404a890c167b50852
Gaabs <ggaabrielagomez@gmail.com>
2023-06-04 00:08:25 -0600

    Vista muestra lista de chefs con diseño

--- burguerQueen/src/app/components/add-chef/add-chef.component.ts
+++ burguerQueen/src/app/components/add-chef/add-chef.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { OrdersService } from '../../services/admin.service';
+import { workersI } from 'src/app/models/workers.interface';
+
 @Component({
   selector: 'app-add-chef',
   templateUrl: './add-chef.component.html',
   styleUrls: ['./add-chef.component.sass']
 })
 export class AddChefComponent {
+  workers: workersI[] = [];
+  filteredWorkers: workersI[] = [];
 
+
+  constructor(private api: OrdersService) { }
+
+  ngOnInit(): void {
+    this.api.getWorkers().subscribe(data => {
+      this.workers = data
+      this.filteredWorkers = data.filter(worker => worker.role === "chef");
+      return
+    })
+  }
 }
