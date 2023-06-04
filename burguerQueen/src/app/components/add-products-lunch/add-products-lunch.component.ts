13438266f7135ebf46b081ef8eb2346eeb7fe58b
Gaabs <ggaabrielagomez@gmail.com>
2023-06-03 22:33:54 -0600

    peticion de productos de comida filtrados con diseño

--- burguerQueen/src/app/components/add-products-lunch/add-products-lunch.component.ts
+++ burguerQueen/src/app/components/add-products-lunch/add-products-lunch.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { OrdersService } from '../../services/admin.service';
+import { productsI } from 'src/app/models/products.interface';
+
 @Component({
   selector: 'app-add-products-lunch',
   templateUrl: './add-products-lunch.component.html',
   styleUrls: ['./add-products-lunch.component.sass']
 })
 export class AddProductsLunchComponent {
+  products:productsI[] = [];
+  filteredProducts: productsI[] = [];
+   
+  constructor(private api:OrdersService) {}
 
+  ngOnInit():void {
+    this.api.getProducts().subscribe(data => {
+      this.products = data
+      this.filteredProducts = data.filter(product => product.type === "Comida");     
+      return
+    })
+  }
 }
