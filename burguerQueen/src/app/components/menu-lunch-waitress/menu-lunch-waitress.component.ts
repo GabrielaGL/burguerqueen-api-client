16ab9e7a70ecbefe89e98594d9202f0e1cc56706
Gaabs <ggaabrielagomez@gmail.com>
2023-06-05 22:15:05 -0600

    Cambiar botones del menu por api

--- burguerQueen/src/app/components/menu-lunch-waitress/menu-lunch-waitress.component.ts
+++ burguerQueen/src/app/components/menu-lunch-waitress/menu-lunch-waitress.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { OrdersService } from '../../services/admin.service';
+import { productsI } from 'src/app/models/products.interface';
+
 @Component({
   selector: 'app-menu-lunch-waitress',
   templateUrl: './menu-lunch-waitress.component.html',
   styleUrls: ['./menu-lunch-waitress.component.sass']
 })
 export class MenuLunchWaitressComponent {
+
+  products:productsI[] = [];
+  filteredProducts: productsI[] = [];
+
+  constructor(private api:OrdersService) {}
+
+  ngOnInit():void {
+    this.api.getProducts().subscribe(data => {
+      this.products = data
+      this.filteredProducts = data.filter(product => product.type === "Comida");     
+      return
+    })
+  }
 
 }
