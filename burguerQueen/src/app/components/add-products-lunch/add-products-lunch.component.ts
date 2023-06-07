82165d4a26de79158755a2c0e909decdcc6ddb33
Gaabs <ggaabrielagomez@gmail.com>
2023-06-06 19:14:32 -0600

    Adición de productos para desayunos y comidas listo

--- burguerQueen/src/app/components/add-products-lunch/add-products-lunch.component.ts
+++ burguerQueen/src/app/components/add-products-lunch/add-products-lunch.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { OrdersService } from '../../services/admin.service';
 import { productsI } from 'src/app/models/products.interface';
@@ -11,14 +12,30 @@ import { productsI } from 'src/app/models/products.interface';
 export class AddProductsLunchComponent {
   products:productsI[] = [];
   filteredProducts: productsI[] = [];
+
+  newProduct = new FormGroup({
+    name: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+    type: new FormControl('Comida'),
+    dateEntry: new FormControl(new Date())
+  })
    
   constructor(private api:OrdersService) {}
 
   ngOnInit():void {
     this.api.getProducts().subscribe(data => {
       this.products = data
-      this.filteredProducts = data.filter(product => product.type === "Comida");     
-      return
+      this.filteredProducts = data.filter(product => product.type === "Comida");
     })
+  }
+
+  addProduct(form:any) {
+    const info:productsI = form;
+    this.api.postProducts(info).subscribe(data => {
+      //TODO: Agregar Modal
+    console.log(data);
+    })    
+    
   }
 }
--- burguerQueen/src/app/components/add-products/add-products.component.ts
+++ burguerQueen/src/app/components/add-products/add-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { OrdersService } from '../../services/admin.service';
 import { productsI } from 'src/app/models/products.interface';
@@ -12,15 +13,31 @@ export class AddProductsComponent {
 
   products:productsI[] = [];
   filteredProducts: productsI[] = [];
+
+  newProduct = new FormGroup({
+    name: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+    type: new FormControl('Desayuno'),
+    dateEntry: new FormControl(new Date())
+  })
    
   constructor(private api:OrdersService) {}
 
   ngOnInit():void {
     this.api.getProducts().subscribe(data => {
       this.products = data
-      this.filteredProducts = data.filter(product => product.type === "Desayuno");     
-      return
+      this.filteredProducts = data.filter(product => product.type === "Desayuno");
     })
+  }
+
+  addProductB(form:any) {
+    const info:productsI = form;
+    this.api.postProducts(info).subscribe(data => {
+      //TODO: Agregar Modal
+    console.log(data);
+    })    
+    
   }
 
 }
