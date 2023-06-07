dcb59977141b097ebe054a1c2b5537c414581daa
Gaabs <ggaabrielagomez@gmail.com>
2023-06-07 14:50:35 -0600

    Función para traer órdenes

--- burguerQueen/src/app/components/kitchen/kitchen.component.ts
+++ burguerQueen/src/app/components/kitchen/kitchen.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { OrdersService } from 'src/app/services/admin.service';
+import { ordersI } from 'src/app/models/orders.interface';
+
 @Component({
   selector: 'app-kitchen',
   templateUrl: './kitchen.component.html',
   styleUrls: ['./kitchen.component.sass']
 })
 export class KitchenComponent {
+  orders:ordersI[] = [];
 
+  constructor(private api:OrdersService) {}
+
+  ngOnInit():void {
+    this.api.getOrders().subscribe(data => {
+      this.orders = data
+      console.log(data);
+      
+    })
+  }
 }
