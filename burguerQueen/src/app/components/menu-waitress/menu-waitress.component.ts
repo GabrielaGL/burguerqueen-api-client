4bbcfbb98676d1f2eef1c8ff5f2eaf09e636d68a
Gaabs <ggaabrielagomez@gmail.com>
2023-06-08 14:53:46 -0600

    Botones de productos agregan valores a las ordenes

--- burguerQueen/src/app/components/menu-waitress/menu-waitress.component.ts
+++ burguerQueen/src/app/components/menu-waitress/menu-waitress.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 import { OrdersService } from '../../services/admin.service';
 import { productsI } from 'src/app/models/products.interface';
+import { ordersI } from 'src/app/models/orders.interface';
 
 @Component({
   selector: 'app-menu-waitress',
@@ -13,13 +16,26 @@ export class MenuWaitressComponent {
   products:productsI[] = [];
   filteredProducts: productsI[] = [];
 
-  constructor(private api:OrdersService) {}
+  orders:ordersI[] = [];
+  cart$ = this.service.cart$
+
+  newOrder = new FormGroup({
+    table: new FormControl('', Validators.required),
+    client: new FormControl('', Validators.required),
+    
+  })
+
+  constructor(private api:OrdersService, private activerouter:ActivatedRoute, private service:OrdersService) {}
 
   ngOnInit():void {
     this.api.getProducts().subscribe(data => {
       this.products = data
       this.filteredProducts = data.filter(product => product.type === "Desayuno");
     })
+  }
+
+  addToCart(product:productsI) {
+    return this.service.addProduct(product);
   }
 
 }
