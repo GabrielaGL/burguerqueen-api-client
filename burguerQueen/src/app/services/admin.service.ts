f2fa29162f8f0dc697906aac71b64b0283c42c8b
Gaabs <ggaabrielagomez@gmail.com>
2023-06-08 14:55:16 -0600

    Agregar funciones para agregar al carrito y hacer nuevo observable para rastrear productos completos

--- burguerQueen/src/app/services/admin.service.ts
+++ burguerQueen/src/app/services/admin.service.ts
@@ -1,52 +1,81 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 import { productsI } from '../models/products.interface';
 import { workersI } from '../models/workers.interface';
 import { responsepostI } from '../models/responsepost';
-import { ordersI, orderqtyI } from '../models/orders.interface';
+import { ordersI } from 'src/app/models/orders.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
   url: string = 'http://localhost:8080/'
+  private cartProducts: productsI[] = [];
+  private orders: ordersI[] = [];
+
+  private cart = new BehaviorSubject<productsI[]>([]);
+  cart$ = this.cart.asObservable();
 
   constructor(private http: HttpClient) { }
 
-  getProducts():Observable<productsI[]> {
+  getProducts(): Observable<productsI[]> {
     let url = this.url + 'products';
     return this.http.get<productsI[]>(url);
   }
 
-  getWorkers():Observable<workersI[]> {
+  getWorkers(): Observable<workersI[]> {
     let url = this.url + 'users';
     return this.http.get<workersI[]>(url);
   }
 
-  postWorker(form:workersI):Observable<responsepostI> {
-    let url = this.url + 'users'; 
-    return this.http.post<responsepostI>(url, form);
-  }
-
-  postProducts(form:productsI):Observable<responsepostI> {
-    let url = this.url + 'products'; 
-    return this.http.post<responsepostI>(url, form);
-  }
-
-  getSingleWorker(id:any):Observable<workersI> {
+  getSingleWorker(id: any): Observable<workersI> {
     let url = this.url + 'users/' + id;
     return this.http.get<workersI>(url);
   }
 
-  getOrders():Observable<ordersI[]> {
+  getOrders(): Observable<ordersI[]> {
     let url = this.url + 'orders';
     return this.http.get<ordersI[]>(url);
   }
 
-  postOrders(form:ordersI):Observable<responsepostI> {
-    let url = this.url + 'orders'; 
+
+  postWorker(form: workersI): Observable<responsepostI> {
+    let url = this.url + 'users';
     return this.http.post<responsepostI>(url, form);
   }
+
+  postProducts(form: productsI): Observable<responsepostI> {
+    let url = this.url + 'products';
+    return this.http.post<responsepostI>(url, form);
+  }
+
+  postOrders(form: ordersI): Observable<responsepostI> {
+    let url = this.url + 'orders';
+    return this.http.post<responsepostI>(url, form);
+  }
+
+
+  addProduct(product: productsI) {
+    if (this.cartProducts.length === 0) {
+      product.qty = 1;
+      this.cartProducts.push(product);
+      this.cart.next(this.cartProducts);
+    } else {
+      let productMod = this.cartProducts.find(element => {
+        return element.id === product.id;
+      })
+      if (productMod) {
+        productMod.qty = productMod.qty + 1
+      } else {
+        product.qty = 1;
+        this.cartProducts.push(product);
+        this.cart.next(this.cartProducts);
+      }
+    }
+
+  }
+
 }
