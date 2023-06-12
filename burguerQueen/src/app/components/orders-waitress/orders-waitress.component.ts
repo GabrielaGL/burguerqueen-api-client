726ffcf1a514cf8fac01aae0ee63f04cd823c414
Gaabs <ggaabrielagomez@gmail.com>
2023-06-11 19:46:41 -0600

    Ordenes se muestran, filtradas por listas y pendientes

--- burguerQueen/src/app/components/orders-waitress/orders-waitress.component.ts
+++ burguerQueen/src/app/components/orders-waitress/orders-waitress.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { OrdersService } from 'src/app/services/admin.service';
+
 import { ordersI } from 'src/app/models/orders.interface';
 
 @Component({
@@ -11,24 +12,30 @@ import { ordersI } from 'src/app/models/orders.interface';
 })
 export class OrdersWaitressComponent {
 
-  orders:ordersI[] = [];
-  filteredOrders:ordersI[] = [];
+  orders: ordersI[] = [];
+  filteredOrders: ordersI[] = [];
+  filteredOrdersReady: ordersI[] = [];
+
 
   newOrder = new FormGroup({
     table: new FormControl('', Validators.required),
     client: new FormControl('', Validators.required),
-    
+
   })
 
-  constructor(private api:OrdersService) {}
+  constructor(private service: OrdersService) { }
 
-  ngOnInit():void {
-    this.api.getOrders().subscribe(data => {
-      this.orders = data
+  ngOnInit(): void {
+    this.service.getOrders().subscribe(data => {
       this.filteredOrders = data.filter(order => order.status === "pending");
-      console.log(data);
-      
+      this.filteredOrdersReady = data.filter(order => order.status === "ready")
+      console.log(this.filteredOrders);
     })
+  }
+
+  totalCart() {
+    const result = this.service.totalCart();
+    return result;
   }
 
 }
