071eac5a3879eeef39974f2adb153436a17ad4bf
Gaabs <ggaabrielagomez@gmail.com>
2023-06-13 16:31:09 -0600

    Agregar orden lista *faltan modales

--- burguerQueen/src/app/components/waitress/menu-waitress/menu-waitress.component.ts
+++ burguerQueen/src/app/components/waitress/menu-waitress/menu-waitress.component.ts
@@ -1,6 +1,8 @@
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { map, switchMap, take } from 'rxjs';
 
 import { OrdersService } from '../../../services/admin.service';
 import { productsI } from 'src/app/models/products.interface';
@@ -12,51 +14,53 @@ import { ordersI } from 'src/app/models/orders.interface';
   styleUrls: ['./menu-waitress.component.sass']
 })
 export class MenuWaitressComponent {
-  
-  products:productsI[] = [];
+
+  products: productsI[] = [];
   filteredProducts: productsI[] = [];
 
-  orders:ordersI[] = [];
+  orders: ordersI[] = [];
   cart$ = this.service.cart$
 
-  idProducts:any = [];
+  date:any = new Date();
+  format:string = 'M/d/yy, h:mm a';
 
   newOrder = new FormGroup({
-    table: new FormControl('', Validators.required),
+    table: new FormControl(0, Validators.required),
     client: new FormControl('', Validators.required),
-    id: new FormControl('', Validators.required),
-    dataEntry: new FormControl(new Date())
+    status: new FormControl('pending'),
   })
 
-  constructor(private service:OrdersService, private activerouter:ActivatedRoute) {}
+  constructor(private service: OrdersService, private activerouter: ActivatedRoute, private datePipe: DatePipe) { }
 
-  ngOnInit():void {
+  ngOnInit(): void {
     this.service.getProducts().subscribe(data => {
       this.products = data
       this.filteredProducts = data.filter(product => product.type === "Desayuno");
     })
   }
 
-  addToCart(product:productsI) {
-    return this.service.addProduct(product);
+  addToCart(product: productsI) {
+    const pro = this.service.addProduct(product);
+    console.log(pro);
+
   }
 
-  totalProduct(price:number, qty:number) {
+  totalProduct(price: number, qty: number) {
     return price * qty;
   }
 
-  deleteProduct(id:number) {
+  deleteProduct(id: number) {
     this.service.deleteProduct(id);
   }
 
-  updateCart(operation:string, id:number) {
+  updateCart(operation: string, id: number) {
     const product = this.service.findIdProduct(id);
     if (product) {
       if (operation === 'min' && product.qty > 0) {
-        product.qty = product.qty -1;
+        product.qty = product.qty - 1;
       }
       if (operation === 'add') {
-        product.qty = product.qty +1;
+        product.qty = product.qty + 1;
       }
       if (product.qty === 0) {
         this.deleteProduct(id);
@@ -71,8 +75,25 @@ export class MenuWaitressComponent {
 
 
   createOrder() {
-    this.idProducts = this.addToCart
-    console.log(this.idProducts);
-    
+    console.log(this.newOrder);
+    if (!this.newOrder.invalid) {
+      this.cart$.pipe(
+        take(1),
+        map((order) => {
+          let dataOrder: ordersI = {
+            table: this.newOrder.controls.table.value,
+            client: this.newOrder.controls.client.value,
+            products: order,
+            status: this.newOrder.controls.status.value,
+            dataEntry: this.datePipe.transform(this.date, this.format),
+            dateProcessed: ''
+          }
+          return dataOrder
+        }),
+        switchMap(dataOrder => {
+          return this.service.postOrders(dataOrder)
+        })
+      ).subscribe()
+    }
   }
 }
