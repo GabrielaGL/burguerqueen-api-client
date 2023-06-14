4b2b8e36076910741d94bd89201ace1608eaf8cf
Gaabs <ggaabrielagomez@gmail.com>
2023-06-13 21:52:30 -0600

    Ordenes de Cocina cambian el status de pendiente a ready, con timer para refrescar la pagina

--- burguerQueen/src/app/components/kitchen/kitchen.component.ts
+++ burguerQueen/src/app/components/kitchen/kitchen.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 import { OrdersService } from 'src/app/services/admin.service';
+import { AlertsService } from 'src/app/services/alerts/alerts.service';
+
 import { ordersI } from 'src/app/models/orders.interface';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-kitchen',
@@ -13,15 +17,48 @@ export class KitchenComponent {
   filteredOrders: ordersI[] = [];
   filteredOrdersReady: ordersI[] = [];
 
-  constructor(private api:OrdersService) {}
+  statusR:{} = {status: 'ready'}
+  statusP:{} = {status: 'pending'}
+
+
+  constructor(private service:OrdersService, private alerts:AlertsService, private router: Router) {}
 
   ngOnInit():void {
-    this.api.getOrders().subscribe(data => {
+    this.service.getOrders().subscribe(data => {
       this.filteredOrders = data.filter(order => order.status === "pending");
       this.filteredOrdersReady = data.filter(order => order.status === "ready")
       this.orders = data
-      console.log(data);
-      
+      console.log(data);      
     })
   }
+
+  statusReady(id:any) {   
+    this.service.patchOrder(id, this.statusR).subscribe({
+      next: (response:any) => {
+        this.alerts.responseSuccess('Pronto vendrán a recogerla, gracias por avisarnos 🍔', '¡La orden está lista!');
+        
+      },
+      error: (error) => {
+        this.alerts.responseError('Parece que ocurrió un error 😥 Acude con tu administrador, el servidor podría estar fallando', 'Error');
+      }
+    })
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
+  }
+
+  statusPending(id:any) {
+    this.service.patchOrder(id, this.statusP).subscribe({
+      next: (response:any) => {
+        this.alerts.responseSuccess('¡No te preocupes! Va de vuelta a la cocina', 'Parece que hubo un error y esta orden no está lista')
+      },
+      error: (error) => {
+        this.alerts.responseError('Parece que ocurrió un error 😥 Acude con tu administrador, el servidor podría estar fallando', 'Error')
+      }
+    })
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
+  }
+ 
 }
